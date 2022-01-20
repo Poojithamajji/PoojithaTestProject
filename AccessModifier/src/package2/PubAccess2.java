be641d312de38a53fda82903526133f90b3342c4
majji.poojitha <majji.poojitha@mphasis.com>
2022-01-20 09:54:29 +0530

    modified public access

--- AccessModifier/src/package2/PubAccess2.java
+++ AccessModifier/src/package2/PubAccess2.java
@@ -4,10 +4,8 @@ public class PubAccess2 {
 
 	public static void main(String[] args) {
 	
-				PubAccess2 obj = new PubAccess2(); 
-		        obj.display();  
+				publicaccess obj = new publicaccess(); 
+		        obj.display();
 		
 	}
-
-
 }

2da7d1355e8b3a643cb546628abe764ef95b9f7e
majji.poojitha <majji.poojitha@mphasis.com>
2022-01-20 11:00:21 +0530

    mofified private access modifier

--- AccessModifier/src/Package1/PrivateAccess.java
+++ AccessModifier/src/Package1/PrivateAccess.java
@@ -1,18 +1,29 @@
 package Package1;
-
-{ 
-   private void display() 
-    { 
-        System.out.println("You are using private access specifier"); 
-    } 
-} 
-
 public class PrivateAccess {
-
-	public static void main(String[] args) {
-				System.out.println("Private Access Specifier");
-				PrivateAccess  obj = new PrivateAccess ();
-
+	  private String fname = "poojitha";
+	  private String lname = "majji";
+	  private String email = "poojitham@gmail.com";
+	  private int age = 23;
+	  
+	  public static void main(String[] args) {
+		  PrivateAccess myObj = new PrivateAccess();
+	    System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+	    System.out.println("Email: " + myObj.email);
+	    System.out.println("Age: " + myObj.age);
+	  }
 	}
 
-}
+//class privateam{
+//private int data= 123;
+//private void  msg () {
+	//System.out.println("Private Access Specifier");
+//}
+//}
+//public class PrivateAccess {
+
+//public static void main(String[] args) {
+				//privateam  obj = new privateam ();
+				//System.out.println(obj.data);
+				//obj.msg();
+		//}
+//}

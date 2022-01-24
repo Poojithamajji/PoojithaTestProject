908be540f374da197da24f374ed903a5f495dc53
majji.poojitha <majji.poojitha@mphasis.com>
2022-01-24 21:02:50 +0530

    modified

--- Assignment/src/javaassignment/ExceptionHandling.java
+++ Assignment/src/javaassignment/ExceptionHandling.java
@@ -8,15 +8,4 @@ package javaassignment;
 				return ("MyException Occurred: "+str1) ;
 			   }
 			}
-		class Example1{
-			   public static void main(String args[]){
-				try{
-					System.out.println("Starting of try block");
-					throw new ExceptionHandling("This is My error Message");
-				}
-				catch(ExceptionHandling exp){
-					System.out.println("Catch Block") ;
-					System.out.println(exp) ;
-				}
-			   }
-		}
+		
